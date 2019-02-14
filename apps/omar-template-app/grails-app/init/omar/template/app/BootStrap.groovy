314f5f391bae939575e3765ef86f3bdcae8619bc
sbortman <scott.bortman@gmail.com>
2019-02-14 15:32:43 -0500

    initial import

---
+++ apps/omar-template-app/grails-app/init/omar/template/app/BootStrap.groovy
@@ -0,0 +1,9 @@
+package omar.template.app
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
---
+++ plugins/omar-template-plugin/grails-app/init/omar/template/plugin/BootStrap.groovy
@@ -0,0 +1,9 @@
+package omar.template.plugin
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
